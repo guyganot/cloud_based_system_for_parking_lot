b1e53ac8dc64ac51c8a93a5b3cef3e71d23ba717
Guy Ganot <105721440+guyganot@users.noreply.github.com>
2023-05-11 16:17:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,93 @@
-import datetime
-import decimal
-import os
+import json
+import time
+from flask import Flask, request, Response
 
-import boto3
-from boto3.dynamodb.conditions import Key
+app = Flask(__name__)
 
-# Initialize the DynamoDB client
-dynamodb = boto3.resource('dynamodb')
-table_name = os.environ.get('TABLE_NAME')
-table = dynamodb.Table(table_name)
+parked_cars = {}
+plate_id_in_lot = set()
+occupied_parking_lot_number = set()
 
-# Define the parking lot entry endpoint
-def parking_lot_entry(event, context):
-    # Parse the license plate and parking lot ID from the request parameters
-    plate = event['queryStringParameters']['plate']
-    parking_lot = event['queryStringParameters']['parkingLot']
-    
-    # Get the current timestamp
-    timestamp = datetime.datetime.now().timestamp()
-    
-    # Create a new parking ticket in DynamoDB
-    response = table.put_item(
-        Item={
-            'ticketId': str(timestamp),
-            'plate': plate,
-            'parkingLot': parking_lot,
-            'entryTime': str(timestamp),
-        }
-    )
-    
-    # Return the ticket ID as the response
-    return {
-        'statusCode': 200,
-        'body': response['Item']['ticketId'],
+@app.route('/entry', methods=['POST'])
+def entry():
+    plate_id = request.form.get('plate')
+    parking_lot_number = request.form.get('parkingLot')
+    if not plate_id or not parking_lot_number:
+        return Response(
+            response=json.dumps({'error': 'Invalid request. Please provide both plate and parkingLot.'}),
+            status=400,
+            mimetype='application/json'
+        )
+    if plate_id in plate_id_in_lot:
+        return Response(
+            response=json.dumps({'error': 'Invalid request. Plate ID is already in the parking lot.'}),
+            status=400,
+            mimetype='application/json'
+        )
+    if parking_lot_number in occupied_parking_lot_number:
+        return Response(
+            response=json.dumps({'error': 'Invalid request. The parking lot number you enter is occupied.'}),
+            status=400,
+            mimetype='application/json'
+        )
+
+    ticket_id = str(int(time.time())) # generate unique ticket id based on timestamp
+    parked_cars[ticket_id] = {
+        'entry_time': time.time(),
+        'plate_id': plate_id,
+        'parking_lot_number': parking_lot_number,
     }
 
-# Define the parking lot exit endpoint
-def parking_lot_exit(event, context):
-    # Parse the ticket ID from the request parameters
-    ticket_id = event['queryStringParameters']['ticketId']
-    
-    # Retrieve the parking ticket from DynamoDB
-    response = table.query(
-        KeyConditionExpression=Key('ticketId').eq(ticket_id)
+    plate_id_in_lot.add(plate_id)
+    occupied_parking_lot_number.add(parking_lot_number)
+    response = {'ticket_id': ticket_id}
+    return Response(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
     )
-    
-    # Calculate the total parked time and charge
-    entry_time = decimal.Decimal(response['Items'][0]['entryTime'])
-    exit_time = decimal.Decimal(datetime.datetime.now().timestamp())
-    parked_time = exit_time - entry_time
-    parked_hours = parked_time / 3600
-    parked_hours_rounded = decimal.Decimal(parked_hours.quantize(decimal.Decimal('.25'), rounding=decimal.ROUND_HALF_UP))
-    charge = parked_hours_rounded * decimal.Decimal(10)
-    
-    # Update the parking ticket with the exit time and charge
-    table.update_item(
-        Key={
-            'ticketId': ticket_id
-        },
-        UpdateExpression='SET exitTime = :val1, charge = :val2',
-        ExpressionAttributeValues={
-            ':val1': str(exit_time),
-            ':val2': str(charge),
-        }
-    )
-    
-    # Return the license plate, parking lot ID, total parked time, and charge as the response
-    return {
-        'statusCode': 200,
-        'body': f"License plate: {response['Items'][0]['plate']}, Parking lot ID: {response['Items'][0]['parkingLot']}, Total parked time: {parked_hours_rounded} hours, Charge: {charge}",
+
+
+@app.route('/exit', methods=['POST'])
+def exit():
+    ticket_id = request.form.get('ticketId')
+    if not ticket_id:
+        return Response(
+            response=json.dumps({'error': 'Invalid request. Please provide ticketId.'}),
+            status=400,
+            mimetype='application/json'
+        )
+
+    if ticket_id not in parked_cars:
+        return Response(
+            response=json.dumps({'error': 'Invalid ticketId. No car found for the given ticketId.'}),
+            status=404,
+            mimetype='application/json'
+        )
+
+    parked_car = parked_cars[ticket_id]
+    entry_time = parked_car['entry_time']
+    parked_time_in_seconds = time.time() - entry_time
+    parked_time_in_minutes = int(parked_time_in_seconds / 60)
+    parked_time_in_hours = parked_time_in_minutes // 60
+    parked_time_in_quarters = (parked_time_in_minutes % 60) // 15
+    total_charge = (parked_time_in_hours * 4 + parked_time_in_quarters) * 2.5
+
+    response = {
+        'plate_id': parked_car['plate_id'],
+        'parked_time': f'{parked_time_in_hours}h {parked_time_in_minutes}m',
+        'charge': f'${total_charge}'
     }
+
+    del parked_cars[ticket_id]
+    plate_id_in_lot.remove(parked_car['plate_id'])
+    occupied_parking_lot_number.remove(parked_car['parking_lot_number'])
+    return Response(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
