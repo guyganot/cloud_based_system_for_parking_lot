b4b53131b7e66186e3e9a6527f2e1e9fd26dc87d
Guy Ganot <105721440+guyganot@users.noreply.github.com>
2023-05-13 12:54:18 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,34 @@
-# debug
-# set -o xtrace
-
-KEY_NAME="cloud-course-$(date +'%s')"
+KEY_NAME="cloud-course-`date +'%N'`"
 KEY_PEM="$KEY_NAME.pem"
 
-echo "Create key pair $KEY_PEM to connect to instances and save locally"
-sudo aws ec2 create-key-pair --key-name "$KEY_NAME" | jq -r ".KeyMaterial" > "$KEY_PEM"
+echo "create key pair $KEY_PEM to connect to instances and save locally"
+aws ec2 create-key-pair --key-name $KEY_NAME \
+    | jq -r ".KeyMaterial" > $KEY_PEM
 
 # secure the key pair
-chmod 400 "$KEY_PEM"
+chmod 400 $KEY_PEM
 
-SEC_GRP="parking-lot-sg-$(date +'%s')"
+SEC_GRP="my-sg-`date +'%N'`"
 
-echo "Setup firewall $SEC_GRP"
-aws ec2 create-security-group --group-name "$SEC_GRP" --description "Access my instances" > /dev/null
-
+echo "setup firewall $SEC_GRP"
+aws ec2 create-security-group   \
+    --group-name $SEC_GRP       \
+    --description "Access my instances" 
 
 # figure out my ip
 MY_IP=$(curl ipinfo.io/ip)
 echo "My IP: $MY_IP"
 
 
-echo "Setup rule allowing SSH access to $MY_IP only"
-aws ec2 authorize-security-group-ingress --group-name "$SEC_GRP" --port 22 --protocol tcp --cidr "$MY_IP"/32 > /dev/null
+echo "setup rule allowing SSH access to $MY_IP only"
+aws ec2 authorize-security-group-ingress        \
+    --group-name $SEC_GRP --port 22 --protocol tcp \
+    --cidr $MY_IP/32
 
-echo "Setup rule allowing HTTP (port 5000) access to $MY_IP only"
-aws ec2 authorize-security-group-ingress --group-name "$SEC_GRP" --port 5000 --protocol tcp --cidr "$MY_IP"/32 > /dev/null
+echo "setup rule allowing HTTP (port 5000) access to all IP"
+aws ec2 authorize-security-group-ingress        \
+    --group-name $SEC_GRP --port 5000 --protocol tcp \
+    --cidr 0.0.0.0/0
 
 UBUNTU_20_04_AMI="ami-00aa9d3df94c6c354"
 
@@ -33,34 +36,28 @@ echo "Creating Ubuntu 20.04 instance..."
 RUN_INSTANCES=$(aws ec2 run-instances   \
     --image-id $UBUNTU_20_04_AMI        \
     --instance-type t2.micro            \
-    --key-name "$KEY_NAME"                \
-    --security-groups "$SEC_GRP")
+    --key-name $KEY_NAME                \
+    --security-groups $SEC_GRP)
 
-INSTANCE_ID=$(echo "$RUN_INSTANCES" | jq -r '.Instances[0].InstanceId')
+INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
 
 echo "Waiting for instance creation..."
-aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids "$INSTANCE_ID" |
+PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID | 
     jq -r '.Reservations[0].Instances[0].PublicIpAddress'
 )
 
 echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
 
-echo "Execute script on machine"
-ssh -i "$KEY_PEM" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@"$PUBLIC_IP" /bin/bash << EOF
-    echo "Updating apt-get..."
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - > /dev/null
-    sudo apt-get update > /dev/null
-    echo "Installing nodejs, npm, git..."
-    sudo apt-get install -y nodejs git > /dev/null
-    git clone https://github.com/guyganot/cloud_based_system_for_parking_lot.git
-    cd parking-lot
-    echo "Runing npm install..."
-    sudo npm install > /dev/null
-    echo "Starting server..."
-    nohup node index.js &>/dev/null &
-    echo "Server up and running!"
-    exit
+echo "deploying code to production"
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" main.py ubuntu@$PUBLIC_IP:/home/ubuntu/
+
+echo "setup production environment"
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
+    sudo apt update
+    sudo apt install python3-flask -y
+    # run app
+    nohup flask run --host 0.0.0.0  &>/dev/null &
     exit
 EOF
